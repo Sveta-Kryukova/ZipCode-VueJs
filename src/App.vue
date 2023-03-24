19c35178440f91b16ac6ab9ad74f76fd5559d789
Sveta Kryukova <kryukova.sv.13@gmail.com>
2023-03-24 13:13:54 +0200

    fixed styles, error message, enter submit

--- src/App.vue
+++ src/App.vue
@@ -9,94 +9,129 @@
       <span></span>
       <span></span>
     </div>
-    
-    <h1 class="display-1">ZIP Code Lookup</h1>
 
-    <form>
-      <label for="zip" class="display-6">ZIP Code:</label>
+    <h1 class="h1">ZIP Code Lookup</h1>
 
-      <input type="text" id="zip" v-model="zipCode" class="form-control" placeholder="Write your ZIP code">
+    <form @submit.prevent="searchZipCode">
+      <label for="zip" class="h4">ZIP Code:</label>
+
+      <input
+        type="text"
+        id="zip"
+        v-model="zipCode"
+        class="form-control"
+        placeholder="Write your ZIP code"
+        style="margin-bottom: 10px"
+        @keydown.enter="searchZipCode"
+      />
+
+      <div v-if="errorMessage">
+        <p class="h4 text-danger">{{ errorMessage }}</p>
+      </div>
+
+      <div class="d-flex justify-content-center">
+        <button
+          type="submit"
+          class="btn btn-outline-secondary"
+          style="width: 600px"
+        >
+          Show ZIP code info
+        </button>
+      </div>
     </form>
 
-    <button type="submit" @click.prevent="searchZipCode" class="btn btn-outline-secondary">Render</button>
-
-    <div v-if="errorMessage">
-      <p class="display-6 text-danger">{{ errorMessage }}</p>
-    </div>
-
     <div v-if="showResults">
-      <p class="display-6"><strong>State:</strong> {{ state }}</p>
+      <p class="h6"><strong>State:</strong> {{ state }}</p>
 
-      <p class="display-6"><strong>City:</strong> {{ city }}</p>
+      <p class="h6"><strong>City:</strong> {{ city }}</p>
     </div>
 
-    <button @click="clear" class="btn btn-outline-secondary">Clear result</button>
+    <div class="d-flex justify-content-center">
+      <button
+        @click="clear"
+        class="btn btn-outline-secondary"
+        style="width: 600px"
+      >
+        Clear result
+      </button>
+    </div>
 
-    <button v-if="showResults" @click="getIPInfo" class="btn btn-outline-secondary">IP Lookup</button>
+    <div class="d-flex justify-content-center">
+      <button
+        v-if="showResults"
+        @click="getIPInfo"
+        class="btn btn-outline-secondary"
+        style="width: 600px"
+      >
+        IP Lookup
+      </button>
+    </div>
 
     <div v-if="ipInfo">
-      <p class="display-6"><strong>ISP:</strong> {{ ipInfo.org }}</p>
+      <p class="h6"><strong>ISP:</strong> {{ ipInfo.org }}</p>
 
-      <p class="display-6"><strong>City:</strong> {{ ipInfo.city }}</p>
+      <p class="h6"><strong>City:</strong> {{ ipInfo.city }}</p>
 
-      <p class="display-6"><strong>Region:</strong> {{ ipInfo.region }}</p>
+      <p class="h6"><strong>Region:</strong> {{ ipInfo.region }}</p>
 
-      <p class="display-6"><strong>Country:</strong> {{ ipInfo.country }}</p>
+      <p class="h6"><strong>Country:</strong> {{ ipInfo.country }}</p>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
-      zipCode: '',
-      state: '',
-      city: '',
+      zipCode: "",
+      state: "",
+      city: "",
       showResults: false,
       ipInfo: null,
-      errorMessage: '',
+      errorMessage: "",
     };
   },
   methods: {
     searchZipCode() {
-      axios.get(`https://api.zippopotam.us/us/${this.zipCode}`)
-        .then(response => {
+      axios
+        .get(`https://api.zippopotam.us/us/${this.zipCode}`)
+        .then((response) => {
           const data = response.data;
           if (data.places.length === 0) {
-            this.errorMessage = 'Invalid ZIP code';
+            this.errorMessage = "Invalid ZIP code";
             this.showResults = false;
             return;
           }
           this.state = data.places[0].state;
-          this.city = data.places[0]['place name'];
+          this.city = data.places[0]["place name"];
           this.showResults = true;
-          this.errorMessage = '';
+          this.errorMessage = "";
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
-          this.errorMessage = 'No such ZIP code, try another one';
+          this.errorMessage = "No such ZIP code, try another one";
           this.showResults = false;
         });
     },
     getIPInfo() {
-      axios.get('https://ipapi.co/json/')
-        .then(response => {
+      axios
+        .get("https://ipapi.co/json/")
+        .then((response) => {
           this.ipInfo = response.data;
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
         });
     },
     clear() {
-      this.zipCode = '';
-      this.state = '';
-      this.city = '';
+      this.zipCode = "";
+      this.state = "";
+      this.city = "";
       this.showResults = false;
       this.ipInfo = null;
-      this.errorMessage = '';
+      this.errorMessage = "";
     },
   },
 };
